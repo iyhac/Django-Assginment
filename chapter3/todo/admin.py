24094106f1bb5ea6dc55ef44b98affc6bddafc23
iyhac <efbjiv@gmail.com>
2025-01-23 16:36:11 +0900

    ✨feat: chapter4 과제 업로드

--- chapter3/todo/admin.py
+++ chapter3/todo/admin.py
@@ -4,13 +4,14 @@ from todo.models import Todo
 
 @admin.register(Todo)
 class TodoAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'is_completed', 'start_date', 'end_date')
+    list_display = ('id', 'user', 'title', 'description', 'is_completed', 'start_date', 'end_date')
     list_filter = ('is_completed',)
     search_fields = ('title',)
     ordering = ('start_date',)
+    list_display_links = ('title',)
     fieldsets = (
         ('Todo Info', {
-            'fields': ('title', 'description', 'is_completed')
+            'fields': ('user', 'title', 'description', 'is_completed')
         }),
         ('Date Range', {
             'fields': ('start_date', 'end_date')
