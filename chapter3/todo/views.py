7a25067b9cc4e5a655b81595e06161ae599f4bfc
iyhac <efbjiv@gmail.com>
2025-02-04 16:15:53 +0900

    ✨feat:chapter6 과제 업로드

--- chapter3/todo/views.py
+++ chapter3/todo/views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.db.models import Q
-from django.http import Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 
